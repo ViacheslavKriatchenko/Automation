a657ce2fddfb2851a48d66f3332318aa8bf2231b
Slavencia <kryatchenko.vv@gmail.com>
2024-06-20 14:14:19 +0300

    добавил новое решение задачи

--- lesson_2/lesson_2_task_9.py
+++ lesson_2/lesson_2_task_9.py
@@ -5,3 +5,9 @@ var3 = var_1 # через переменную
 var_1 = var_2
 var_2 = var3
 print(var_1, var_2, sep=',')
+
+# замена переменных
+var_1 = 37
+var_2 = 99
+var_1, var_2 = var_2, var_1
+print(var_1, var_2, sep=',')
