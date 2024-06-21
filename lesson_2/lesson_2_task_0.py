b190d687ffb1ee043bfd15d1af71d4cd10364497
Slavencia <kryatchenko.vv@gmail.com>
2024-06-21 13:32:54 +0300

    правка

--- lesson_2/lesson_2_task_0.py
+++ lesson_2/lesson_2_task_0.py
@@ -1,9 +1,10 @@
 my_age = input()
 print(type(my_age))  # str
-print(my_age)
-my_age = my_age + 1  # переменная с типом строка + число
-print(my_age)
+print('Ваш возраст ', my_age)  # str
+my_age = my_age + 1  # переменная с типом строка + число = ошибка
+print('Ваш возраст ', my_age)  # ошибка
 
 # преобразовать тип данных при вводе
 my_age = int(input('Введите свой возраст: '))
+my_age = my_age + 1
 print(f'Ваш возраст: {my_age}')
