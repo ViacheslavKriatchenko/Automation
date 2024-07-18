616adc827c6e4d6cae2f792c54b1a0c9f51569fa
Slavencia <kryatchenko.vv@gmail.com>
2024-07-18 20:31:26 +0300

    внес описание

--- lesson_6/click_button_EC.py
+++ lesson_6/click_button_EC.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
 
+# Создание экземпляра вэбдрайвера
 options = Options()
 options.add_argument("--window-size=1920,1080")
 options.add_argument("--ignore-certificate-errors")
@@ -14,18 +15,23 @@ service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
 
 wait = WebDriverWait(driver, 20, poll_frequency=1)
-URL = "http://uitestingplayground.com/ajax"
 
+# Переход на вэб-страницу
+URL = "http://uitestingplayground.com/ajax"
 driver.get(URL)
 
+# Ожидание загрузки страницы
 wait.until(EC.url_to_be((URL)))
 
+# Ожидание кликабельности кнопки и нажатие
 AJAX_BUTTON = ("xpath", "//button[@id='ajaxButton']")
 wait.until(EC.element_to_be_clickable((AJAX_BUTTON))).click()
 
+# Ожидание появления элемента и изъятие текста
 GREEN_FIELD = ("xpath", "//p[@class='bg-success']")
 GREEN_FIELD_TEXT = wait.until(
     EC.visibility_of_element_located((GREEN_FIELD))
     ).text
 
+# Вывод на печать информации
 print(f'Текст поля - {GREEN_FIELD_TEXT}')
--- lesson_6/wait_picture.py
+++ lesson_6/wait_picture.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
 
+# Создание экземпляра вэбдрайвера
 options = Options()
 options.add_argument("--window-size=1920,1080")
 
@@ -13,14 +14,18 @@ service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
 
 wait = WebDriverWait(driver, 20, poll_frequency=1)
-URL = "https://bonigarcia.dev/selenium-webdriver-java/loading-images.html"
 
+# Переход на вэб-страницу
+URL = "https://bonigarcia.dev/selenium-webdriver-java/loading-images.html"
 driver.get(URL)
 
+# Ожидание загрузки элементов пока крутится спиннер
 SPINNER = ("xpath", "//span[@id='spinner']")
 wait.until(EC.invisibility_of_element_located((SPINNER)))
 
+# Вытаскивам информацию из атрибута
 THIRD_PICTURE = ("xpath", "(//div[@id='image-container']//img)[3]")
 THIRD_PICTURE_DATA = driver.find_element(*THIRD_PICTURE).get_attribute("src")
 
+# Вывод на печать информации
 print(f'Содержимое атрибута src - {THIRD_PICTURE_DATA}')
