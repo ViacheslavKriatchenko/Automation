02a6e9ebc7c91d102d10e58c84846450fba87640
Slavencia <90188398+ViacheslavKriatchenko@users.noreply.github.com>
2024-08-04 04:11:32 +0300

    Update task_3_test.py
    убрал слипы

--- lesson_6.1/tests/task_3_test.py
+++ lesson_6.1/tests/task_3_test.py
@@ -1,6 +1,5 @@
 import pytest
 from base.base_test import BaseTest
-from time import sleep
 
 
 class TestTotalPrice(BaseTest):
@@ -10,26 +9,17 @@ class TestTotalPrice(BaseTest):
 
         self.task_3.open()
         self.task_3.is_opened()
-        sleep(2)
         self.task_3.enter_login()
         self.task_3.enter_password()
-        sleep(2)
         self.task_3.click_login_button()
-        sleep(2)
         self.task_3.add_to_cart_Sauce_Labs_Backpack()
         self.task_3.add_to_cart_Sauce_Labs_Bolt_T_Shirt()
         self.task_3.add_to_cart_Sauce_Labs_Onesie()
-        sleep(2)
         self.task_3.go_to_the_cart()
-        sleep(2)
         self.task_3.click_checkout_button()
-        sleep(2)
         self.task_3.enter_firstname('Slava')
         self.task_3.enter_lastname('I\'m doing magic')
         self.task_3.enter_postalcode(12345)
-        sleep(2)
         self.task_3.click_continue_button()
-        sleep(2)
         self.task_3.print_total_price()
-        sleep(2)
         self.task_3.result_verification()
